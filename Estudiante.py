b5eb35447c4df96160b663f0850a7d7de21f0946
Mitchell Rodríguez <mitrodle@gmail.com>
2022-01-19 11:02:14 -0500

    add: Herencia

---
+++ Estudiante.py
@@ -0,0 +1,32 @@
+from Persona import Persona
+
+class Estudiante(Persona):
+    listaEstudiantes = []
+    def __init__(self, nombre, apellido, id, curso):
+        super().__init__(nombre, apellido)
+        self.id = id
+        self.curso = curso
+        self.calificaciones = []
+        
+        Estudiante.listaEstudiantes.append(self)
+    
+    # Propio de Estudiante
+    def asignaCalificacion(self, nota):
+        self.calificaciones.append(nota)
+
+    # Sobreescritura de método
+    def informacion(self):
+        super().informacion()
+        print("Calificaciones", self.calificaciones)
+    
+    # Nuevo método de Estudiante
+    # La intención es tener uno u otro
+    # Como ya se sobreescribió arriba, al llamar informacion() ya está editado
+    # def informacionEstudiante(self):
+    #     self.informacion()
+    #     print("Calificaciones", self.calificaciones)
+
+    @classmethod
+    def imprimeListaEstudiantes(cls):
+        for estudiante in cls.listaEstudiantes:
+            estudiante.informacion()
--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from Persona import Persona
+from Estudiante import Estudiante
+
+# alex = Estudiante("Alex", "Martinez", 132456, "Fundamentos de la web")
+# alex.informacion()
+# alex.asignaCalificacion(8.9)
+# alex.informacion() # Hereda del padre y sóo tiene nombre y apellido
+# Pero podemos crear un nuevo método o sobreescribir el método del padre
 
 print("***** MENU *****")
 print("1. Agregar una persona")
 print("2. Mostrar lista de personas")
-print("3. Terminar el programa")
+print("3. Agregar un estudiante")
+print("4. Mostrar lista de estudiantes")
+print("5. Terminar el programa")
 
 opcion = input("Opción a elegir: ")
 
-while opcion != "3":
+while opcion != "5":
     if opcion == "1":
         nombre = input("Dame el nombre de la persona: ")
         apellido = input("Dame el apellido de la persona: ")
@@ -15,13 +24,24 @@ while opcion != "3":
     elif opcion == "2":
         print("Mostrando la lista de personas")
         Persona.imprimeListaPersonas()
+    elif opcion == "3":
+        nombre = input("Dame el nombre del estudiante: ")
+        apellido = input("Dame el apellido del estudiante: ")
+        id = input("Dame el id del estudiante: ")
+        curso = input("Dame el curso del estudiante: ")
+        nuevoEstudiante = Estudiante(nombre, apellido, int(id), curso)
+    elif opcion == "4":
+        print("Mostrando la lista de estudiantes")
+        Estudiante.imprimeListaEstudiantes()
     else:
         print("Opción incorrecta")
     
     print("***** MENU *****")
     print("1. Agregar una persona")
     print("2. Mostrar lista de personas")
-    print("3. Terminar el programa")
+    print("3. Agregar un estudiante")
+    print("4. Mostrar lista de estudiantes")
+    print("5. Terminar el programa")
 
     opcion = input("Opción a elegir: ")
         
